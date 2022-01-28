2406e95768d32a7e400c5831d03b1fdef06e4786
wjdtngh950 <wjdtngh950@naver.com>
2022-01-28 23:07:28 +0900

    1

---
+++ cmake-build-debug/CMakeFiles/Choice_to_buy_stock.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Choice_to_buy_stock.dir/main.cpp.o"
+  "Choice_to_buy_stock"
+  "Choice_to_buy_stock.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Choice_to_buy_stock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Choice_to_buy_stock.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Choice_to_buy_stock.
+# This may be replaced when dependencies are built.
